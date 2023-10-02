17c5ed6cacdd52e8cae27c0c35297779636430a4
Emmy Steven <hello@emmysteven.com>
2023-10-02 15:11:07 +0100

    temporary disable virtual threads

--- src/main/java/codaholic/configa/Application.java
+++ src/main/java/codaholic/configa/Application.java
@@ -2,29 +2,22 @@ package codaholic.configa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableConfigServer
-@ConditionalOnProperty(value = "spring.thread-executor", havingValue = "virtual")
+//@ConditionalOnProperty(value = "spring.thread-executor", havingValue = "virtual")
 public class Application {
 
-	@Bean
-	public AsyncTaskExecutor asyncTaskExecutor() {
-		return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
-	}
-
-	@Bean
-	public TomcatProtocolHandlerCustomizer<?> protocolHandlerCustomizer() {
-		return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-	}
+//	@Bean
+//	public AsyncTaskExecutor asyncTaskExecutor() {
+//		return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+//	}
+//
+//	@Bean
+//	public TomcatProtocolHandlerCustomizer<?> protocolHandlerCustomizer() {
+//		return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
